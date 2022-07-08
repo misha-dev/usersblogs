86104b3c8776a2e3ad98452093a0bced68fcb9ba
Misha-dev <shabatinmichael@mail.ru>
2022-07-08 17:13:30 +0500

    authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { Authentication } from "./components/Authentication/Authentication";
 import { Layout } from "./components/Layout/Layout";
+import { Register } from "./components/Register/Register";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="usersblogs" element={<Layout />}>
           <Route index element={<Authentication />} />
+          <Route path="login" element={<Authentication />} />
+          <Route path="register" element={<Register />} />
         </Route>
       </Routes>
     </div>
--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import cl from "./Authentication.module.scss"
+import { Link } from "react-router-dom";
+import cl from "./Authentication.module.scss";
 
 export const Authentication = () => {
   return (
-    <div className={cl.authContent}>Authentication</div>
-  )
-}
+    <div className={cl.authContent}>
+      <div className={cl.auth_register_login_wrapper}>
+        <Link to={"/usersblogs/login"} className={cl.auth_login}>
+          Login
+        </Link>
+        <Link to={"/usersblogs/register"} className={cl.auth_register}>
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+};
