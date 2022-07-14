c050e851f7f9f7f2bcd4c031b91cded79befd683
Misha-dev <shabatinmichael@mail.ru>
2022-07-14 14:26:57 +0500

    moved to form html for login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -10,56 +10,58 @@ export const Login = () => {
   const dispatch = useAppDispatch();
   return (
     <div className={cl.loginContentWrapper}>
-      <div className={cl.loginWrapper}>
-        <div className={cl.loginInputs}>
-          <div className="inputWrapper">
-            <input
-              onChange={(e) => {
-                setEmail(e.currentTarget.value);
-              }}
-              value={email}
-              autoCorrect="false"
-              autoComplete="false"
-              id="email"
-              required
-              type="text"
-            />
-            <label htmlFor="email">Email</label>
+      <form action="#">
+        <div className={cl.loginWrapper}>
+          <div className={cl.loginInputs}>
+            <div className="inputWrapper">
+              <input
+                onChange={(e) => {
+                  setEmail(e.currentTarget.value);
+                }}
+                value={email}
+                autoCorrect="false"
+                autoComplete="false"
+                id="email"
+                required
+                type="text"
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="inputWrapper">
+              <input
+                onChange={(e) => {
+                  setPassword(e.currentTarget.value);
+                }}
+                value={password}
+                autoCorrect="false"
+                autoComplete="false"
+                id="password"
+                required
+                type="password"
+              />
+              <label htmlFor="password">Password</label>
+            </div>
           </div>
-          <div className="inputWrapper">
-            <input
-              onChange={(e) => {
-                setPassword(e.currentTarget.value);
-              }}
-              value={password}
-              autoCorrect="false"
-              autoComplete="false"
-              id="password"
-              required
-              type="password"
-            />
-            <label htmlFor="password">Password</label>
-          </div>
+          <button
+            onClick={() => {
+              signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                  const { uid, email, displayName, photoURL } = userCredential.user;
+                  if (displayName && email && photoURL) {
+                    dispatch(setUser({ uid, email, displayName, photoURL }));
+                  }
+                })
+                .catch((error) => {
+                  alert("Invalid password or email");
+                  setPassword("");
+                });
+            }}
+            className={cl.loginButton}
+          >
+            Login
+          </button>
         </div>
-        <button
-          onClick={() => {
-            signInWithEmailAndPassword(auth, email, password)
-              .then((userCredential) => {
-                const { uid, email, displayName, photoURL } = userCredential.user;
-                if (displayName && email && photoURL) {
-                  dispatch(setUser({ uid, email, displayName, photoURL }));
-                }
-              })
-              .catch((error) => {
-                alert("Invalid password or email");
-                setPassword("");
-              });
-          }}
-          className={cl.loginButton}
-        >
-          Login
-        </button>
-      </div>
+      </form>
     </div>
   );
 };
