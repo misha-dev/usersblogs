63a6cd166b7cab5d18c54ba60c19b938d4bf3775
Misha-dev <shabatinmichael@mail.ru>
2022-07-15 17:55:30 +0500

    added full forom for adding post

--- src/components/AddPost/AddPost.tsx
+++ src/components/AddPost/AddPost.tsx
@@ -5,11 +5,25 @@ import { PostImage } from "./PostImage/PostImage";
 export const AddPost = () => {
   const [imageFile, setImageFile] = useState<File>(null!);
   const [htmlImageReader, setHtmlImageReader] = useState<string>(null!);
+  const [postText, setPostText] = useState("");
   return (
     <div className={cl.addPostContentWrapper}>
       <div className={cl.addPostWrapper}>
         <div className={cl.addPostBox}>
           <PostImage htmlImageReader={htmlImageReader} setHtmlImageReader={setHtmlImageReader} imageFile={imageFile} setImageFile={setImageFile} />
+          <textarea
+            autoComplete="off"
+            autoCorrect="off"
+            value={postText}
+            onChange={(e) => {
+              setPostText(e.currentTarget.value);
+            }}
+            className={cl.postText}
+            placeholder="Enter a text"
+          ></textarea>
+          <div className={cl.previewWrapper}>
+            <div className={cl.preview}>Preview</div>
+          </div>
         </div>
       </div>
     </div>
