111ba05e5c23f9666f83b603f966ebd2dc6df1e8
Michael <michaelshabatin@mail.ru>
2022-07-31 01:09:25 +0500

    added animated menu onScroll

--- src/components/Layout/Menu/Menu.tsx
+++ src/components/Layout/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import { signOut } from "firebase/auth";
+import { useEffect, useRef } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import { auth } from "../../../firebase/config";
@@ -8,11 +9,28 @@ import cl from "./Menu.module.scss";
 
 export const Menu = () => {
   const [user] = useAuthState(auth);
+  const menuRef = useRef<HTMLDivElement>(null!);
+
+  const onScrollColorMenu = (e: Event) => {
+    const scrolled = window.scrollY;
+    if (scrolled !== 0) {
+      menuRef.current.classList.add(cl.coloredMenu);
+    } else {
+      menuRef.current.classList.remove(cl.coloredMenu);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScrollColorMenu);
+    return () => {
+      window.removeEventListener("scroll", onScrollColorMenu);
+    };
+  }, []);
 
   const dispatch = useAppDispatch();
   return (
     <nav style={{ height: "73.59px" }}>
-      <div style={{ width: "100%" }} className={cl.menu}>
+      <div ref={menuRef} style={{ width: "100%" }} className={cl.menu}>
         <Link to={"/usersblogs"}>
           <p>Users blogs</p>
         </Link>
