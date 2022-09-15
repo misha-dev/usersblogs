3e4e557b8cca02d485eb9324af3cadfefd21501d
Misha-dev <shabatinmichael@mail.ru>
2022-09-15 23:56:39 +0500

    added props test for post component

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -6,15 +6,15 @@ import { BrowserRouter } from "react-router-dom";
 import * as ReduxHooks from "../../store/hooks";
 import { Post } from "./Post";
 
-describe("Testing Post", () => {
+describe("Post", () => {
   const mockedSelector = jest.spyOn(ReduxHooks, "useAppSelector");
   const mockedUseNavigate = jest.spyOn(router, "useNavigate");
-
   beforeEach(() => {
     mockedSelector.mockReturnValue("test");
     mockedUseNavigate.mockImplementation(jest.fn());
   });
-  test("should Post render", () => {
+
+  it("should Post render", () => {
     render(
       <BrowserRouter>
         <Post
@@ -26,7 +26,6 @@ describe("Testing Post", () => {
           text="Cosmos"
           uid="1"
           userName="Misha"
-          data-testid="123"
           userPhotoURL="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png"
         />
       </BrowserRouter>
@@ -34,7 +33,39 @@ describe("Testing Post", () => {
 
     const post = screen.getByTestId("postContainer");
     const username = screen.getByText(/Misha/i);
+    const likes = screen.getByText(/2 likes/i);
     expect(post).toBeInTheDocument();
     expect(username).toBeInTheDocument();
+    expect(likes).toBeInTheDocument();
+  });
+
+  it("should props render", () => {
+    render(
+      <BrowserRouter>
+        <Post
+          id="1"
+          createdAt={Timestamp.now()}
+          isPreview={false}
+          likes={["misha", "masha"]}
+          postPhotoURL="https://www.reviewofreligions.org/wp-content/uploads/2020/08/shutterstock_557126443-scaled.jpg"
+          text="Cosmos"
+          uid="1"
+          userName="Misha"
+          userPhotoURL="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png"
+        />
+      </BrowserRouter>
+    );
+
+    const username = screen.getByText(/Misha/i);
+    const likes = screen.getByText(/2 likes/i);
+    const [userPhoto, postPhoto] = screen.getAllByRole("img");
+    expect(username).toBeInTheDocument();
+    expect(likes).toBeInTheDocument();
+    expect(userPhoto).toHaveAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png");
+    expect(postPhoto).toHaveAttribute("src", "https://www.reviewofreligions.org/wp-content/uploads/2020/08/shutterstock_557126443-scaled.jpg");
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 });
