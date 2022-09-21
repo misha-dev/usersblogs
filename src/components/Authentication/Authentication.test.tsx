09bc888cbd9076a7b8c1dc8766767f3c235eb7b3
Misha-dev <shabatinmichael@mail.ru>
2022-09-22 01:09:24 +0500

    added navigation test for Authentication component

--- src/components/Authentication/Authentication.test.tsx
+++ src/components/Authentication/Authentication.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import { BrowserRouter, Router } from "react-router-dom";
 import { Authentication } from "./Authentication";
 
 describe("Authentication", () => {
@@ -11,6 +13,29 @@ describe("Authentication", () => {
     const registerButton = screen.getByText(/register/i);
     expect(loginButton).toBeInTheDocument();
     expect(registerButton).toBeInTheDocument();
+  });
+
+  it("should navigate to login page", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <Authentication />
+      </Router>
+    );
+    const loginButton = screen.getByText(/login/i);
+    userEvent.click(loginButton);
+    expect(history.location.pathname).toBe("/usersblogs/login");
+  });
+  it("should navigate to register page", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <Authentication />
+      </Router>
+    );
+    const loginButton = screen.getByText(/register/i);
+    userEvent.click(loginButton);
+    expect(history.location.pathname).toBe("/usersblogs/register");
   });
 
   afterAll(() => {
