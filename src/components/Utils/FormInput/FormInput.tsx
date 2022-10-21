1cffcc2979a71d15fe09e4bb81b31b411a546732
Misha-dev <shabatinmichael@mail.ru>
2022-10-21 19:00:26 +0500

    fixed capitalizing in form input

--- src/components/Utils/FormInput/FormInput.tsx
+++ src/components/Utils/FormInput/FormInput.tsx
@@ -5,7 +5,7 @@ import cl from "./FormInput.module.scss";
 export const FormInput = ({ handler, id, name, placeholder, type }: InputType) => {
   return (
     <div className={cl.inputWrapper}>
-      <input onChange={handler.onChange} value={handler.value} onBlur={handler.onBlur } autoCorrect="false" autoComplete="false" id={id} name={name} required type={type} />
+      <input onChange={handler.onChange} value={handler.value} onBlur={handler.onBlur} autoCorrect="off" autoComplete="off" autoCapitalize={"off"} id={id} name={name} required type={type} />
       <label htmlFor={id}>{placeholder}</label>
     </div>
   );
