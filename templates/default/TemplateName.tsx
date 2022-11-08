743dd3dddb9b85b9fe4497df618ee7fbf0c2ee71
Misha-dev <shabatinmichael@mail.ru>
2022-11-08 20:20:53 +0500

    added template for creating new component

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,7 @@
+import cl from "./TemplateName.module.scss";
+
+interface TemplateNameProps {}
+
+export const TemplateName = ({}: TemplateNameProps) => {
+  return <div className={cl.templateNameWrapper}>TemplateName</div>;
+};
